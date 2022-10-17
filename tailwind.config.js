ff0e7aa0060a9a8abdaf24ac5c4cd5bdde7e87b1
DEREPIERRE Alexandre <derepierre.alexandre@gmail.com>
2022-10-17 21:44:27 +0200

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        "dark_orange": "#E7C169",
-        "brown": "#836110",
-        "dark_red": "#E3473B",
-        "light_grey": "#E8E8E8",
-        "darker_grey": "#171717",
-        "lighter_grey": "#D6D7D8",
-        "dark_grey": "#B9B9BA",
-        "light_black": "#090909",
-        "white_0.4": "rgba(255,255,255,0.4)",
-        "darky_grey": "#373636"
+        "lighter_gray": "#23252b",
+        "orange_crunchyroll": "#f47521",
+        "white": "#FFFFFF",
+        "light_grey": "#DADADA",
+        "light_black": "#141519"
       },
       fontFamily: {
         sans: ['Lato', ...defaultTheme.fontFamily.sans],
-        'FontAwesome': ['FontAwesome'],
-        'Geomanist': ['Geomanist', 'sans-serif'],
-        'Asap': ['Asap', 'sans-serif'],
-        'Geomanist_medium': ['Geomanist_medium', 'sans-serif']
+        'Lato': ['Lato', 'Helvetica Neue', 'helvetica', 'sans-serif']
       },
       screens: {
         xxl: '1410px',
@@ -39,9 +31,6 @@ module.exports = {
         '97': '31.25rem'
       },
       backgroundImage:{
-        'slider': "url('/static/wakanime/637826068437684187Unification-Slide-Main_Waka-CR_1620x350_FR.jpg')",
-        'mini_slider': "url('/static/wakanime/637826068438465635Unification-Thumbnail_Waka-CR_140x74.jpg')",
-        'mini_cta': "url('/static/wakanime/cta-homepage-free-trial.png')"
       },
       boxShadow: {
         'inner_top_black': 'inset 0 11px 8px -11px rgba(0, 0, 0, 0.5)',
